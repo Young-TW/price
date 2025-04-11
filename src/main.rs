1904a8c0889a93317f0370f9044182c2be97550f
Young <young20050727@gmail.com>
2025-04-11 09:43:21 +0800

    feat: use stream in main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,16 @@ mod config;
 mod get;
 
 mod calculate;
+mod stream;
 
 #[tokio::main]
 async fn main() {
+    /*
     println!(
         "total invesment value: {}",
         calculate::calculate_total().await
     );
+    */
+
+    stream::stream(5).await;
 }
